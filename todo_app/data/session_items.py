f4085da0467ffb485aa37ec50c4920e7b3fe6f04
Alexander Chamberlain <alexander.chamberlain@softwire.com>
2022-07-29 12:48:33 +0100

    Complete exercise 1

--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -1,8 +1,8 @@
 from flask import session
 
 _DEFAULT_ITEMS = [
-    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
-    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items', 'complete': False, 'description': 'A task!' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added', 'complete': True }
 ]
 
 
@@ -13,7 +13,7 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
-    return session.get('items', _DEFAULT_ITEMS.copy())
+    return sorted(session.get('items', _DEFAULT_ITEMS.copy()), key=lambda x: x.get('complete'))
 
 
 def get_item(id):
@@ -30,7 +30,7 @@ def get_item(id):
     return next((item for item in items if item['id'] == int(id)), None)
 
 
-def add_item(title):
+def add_item(title, description):
     """
     Adds a new item with the specified title to the session.
 
@@ -43,9 +43,10 @@ def add_item(title):
     items = get_items()
 
     # Determine the ID for the item based on that of the previously added item
-    id = items[-1]['id'] + 1 if items else 0
+    # id = items[-1]['id'] + 1 if items else 0
+    id = max([item['id'] for item in items]) + 1 if items else 0
 
-    item = { 'id': id, 'title': title, 'status': 'Not Started' }
+    item = { 'id': id, 'title': title, 'description': description, 'status': 'Not Started', 'complete': False }
 
     # Add the item to the list
     items.append(item)
@@ -67,3 +68,21 @@ def save_item(item):
     session['items'] = updated_items
 
     return item
+
+def delete_item(id):
+    """
+    Deletes the saved item with the specified ID.
+
+    Args:
+        id: The ID of the item.
+
+    Returns:
+        item: None.
+    """
+
+    existing_items = get_items()
+    print(existing_items)
+    updated_items = [existing_item for existing_item in existing_items if existing_item['id'] != id]
+    print(updated_items)
+
+    session['items'] = updated_items
