ea9daad01d6ed53409d2fb8b0c9953cfc879b0a9
achamberlain999 <67910333+achamberlain999@users.noreply.github.com>
2022-08-26 14:02:00 +0100

    Exercise 1 (#1)
    * Replace secret key
    * Complete exercise 1
    * Markups
    * Improve responsiveness
    * Move new task form into reusable component
    * Add task form
    * Undo change to .env SECRET_KEY
    * Add required attribute

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from todo_app.data.session_items import get_items, add_item, get_item, save_item, delete_item
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,40 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    completed_items = [item for item in items if item['complete']]
+    uncompleted_items = [item for item in items if not item['complete']]
+
+    return render_template('index.html', completed_items=completed_items, uncompleted_items=uncompleted_items)
+
+@app.route('/task', methods=['POST'])
+def add_new_task():
+    title = request.form.get('new_task_title')
+    description = request.form.get('new_task_description')
+    
+    if title:
+        add_item(title, description)
+
+    return redirect('/')
+
+@app.route('/task/complete/<id>', methods=['POST'])
+def complete(id):
+    item = get_item(id)
+    item['complete'] = True
+    save_item(item)
+
+    return redirect('/')
+
+@app.route('/task/uncomplete/<id>', methods=['POST'])
+def uncomplete(id):
+    item = get_item(id)
+    item['complete'] = False
+    save_item(item)
+
+    return redirect('/')
+
+@app.route('/task/delete/<id>', methods=['POST'])
+def delete(id):
+    delete_item(int(id))
+
+    return redirect('/')
