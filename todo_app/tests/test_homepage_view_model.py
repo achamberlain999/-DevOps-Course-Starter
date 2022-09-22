63bfe739c3e993ab59b363f7ae097e33101d2258
Alexander Chamberlain <alexander.chamberlain@softwire.com>
2022-09-22 19:59:02 +0100

    Add failing test for getting completed items

--- todo_app/tests/test_homepage_view_model.py
+++ todo_app/tests/test_homepage_view_model.py
@@ -2,9 +2,17 @@ from todo_app.view_models.homepage_view_model import HomepageViewModel
 from todo_app.models.item import Item
 
 class TestHomepageViewModel:
-    def test_one(self):
+    def test_get_items(self):
         item = Item('ID', 'item_name', 'item_description', complete=False)
 
         model = HomepageViewModel([item])
 
         assert model.completed_items[0] == item
+    
+    def test_get_completed_items(self):
+        uncompleted_item = Item('ID', 'item_name', 'item_description', complete=False)
+        completed_item = Item('ID', 'item_name', 'item_description', complete=True)
+
+        model = HomepageViewModel([uncompleted_item, completed_item])
+
+        assert model.completed_items[0] == completed_item
